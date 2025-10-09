354ede95babd6536a13324ecdb21e92718103601
unknown <vaishnavidhavan026@gmail.com>
2025-10-09 19:32:28 +0530

    Custom hook is created

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,21 +1,10 @@
+import useNowPlaying from "../hooks/useNowPlaying"
 import Header from "./Header"
-import { useEffect } from "react"
-import { TMDB_HEADERS } from "../utils/Constants"
-import { useDispatch } from "react-redux"
-import { addNowPlayingMovies } from "../utils/store/reducers/MovieSlice"
 
 const Browser = () => {
-  const dispatch = useDispatch()
   
-  const getNowPlayingMovies = async() => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',TMDB_HEADERS)
-    const json = await data.json()
-    dispatch(addNowPlayingMovies(json.results))
-  }
-
-  useEffect(() => {
-    getNowPlayingMovies()
-  },[])
+  useNowPlaying()
+  
   
 
     return(
